390a3becbdfa9346a8ddc75a6c84128849af751b
dtorannpu <dtorannpu@gmail.com>
2024-04-14 09:03:11 +0900

    バージョン管理に追加

---
+++ src/main/kotlin/com/example/metricssample/MetricsSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.metricssample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MetricsSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MetricsSampleApplication>(*args)
+}
